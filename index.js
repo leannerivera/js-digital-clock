2f40872b4cdafc1d4a5ad3e2f153c0df9723f554
Leanne Rivera <rivera.leanne+ada@gmail.com>
2018-11-16 15:22:08 -0800

    can clock

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Your code here
+$(document).ready(function() {
+
+    let currentTime = new Date();
+
+    let hours = currentTime.getHours();
+    let minutes = currentTime.getMinutes();
+    let seconds = currentTime.getSeconds();
+    let timePeriod = "AM"
+
+    if (hours >= 12) {
+      hours = hours -12;
+      timePeriod = "PM"
+    }
+
+    if (minutes < 10) {
+      minutes = `0${minutes}`
+    }
+
+    $('#clock').append(`${hours}:${minutes}:${seconds} ${timePeriod}`);
+
+
+    // setInterval(seconds, 1000);
+});
