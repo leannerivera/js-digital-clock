8125858461141e4ac0f84967822077841fd361d6
Leanne Rivera <rivera.leanne+ada@gmail.com>
2018-11-16 15:55:30 -0800

    can seconds update

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
 // Your code here
 $(document).ready(function() {
 
-    let currentTime = new Date();
+pacificTimeClock();
+setInterval(pacificTimeClock,1000);
 
-    let hours = currentTime.getHours();
-    let minutes = currentTime.getMinutes();
-    let seconds = currentTime.getSeconds();
-    let timePeriod = "AM"
-
-    if (hours >= 12) {
-      hours = hours -12;
-      timePeriod = "PM"
-    }
-
-    if (minutes < 10) {
-      minutes = `0${minutes}`
-    }
-
-    $('#clock').append(`${hours}:${minutes}:${seconds} ${timePeriod}`);
-
-
-    // setInterval(seconds, 1000);
 });
+
+function lessThanTen(number) {
+  if (number < 10) {
+    number = `0${number}`;
+    return number;
+  }
+  return number;
+}
+
+function pacificTimeClock() {
+  let currentTime = new Date();
+  let hours = lessThanTen(currentTime.getHours());
+  let minutes = lessThanTen(currentTime.getMinutes());
+  let seconds = lessThanTen(currentTime.getSeconds());
+  let timePeriod = "AM"
+
+  if (hours >= 12) {
+    hours = lessThanTen(hours - 12);
+    timePeriod = "PM"
+  }
+
+  $('#clock').html(`${hours}:${minutes}:${seconds} ${timePeriod}`);
+}
